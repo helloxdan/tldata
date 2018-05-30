fe3162939ee3fd8c560284b2e95819a207836f54
helloxdan <helloxdan@qq.com>
2018-05-30 10:39:07 +0800

    Signed-off-by: helloxdan <helloxdan@qq.com>

--- src/main/java/org/telegram/InputTest.java
+++ src/main/java/org/telegram/InputTest.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class InputTest {
 	public static void main(String[] args) {
-		gotoCmd();
+		// gotoCmd();
+		InputTest i = new InputTest();
+		i.gotoCmd();
 	}
-	private static  void gotoCmd() {
+
+	private void gotoCmd() {
 		Scanner input = new Scanner(System.in);
 		String val = null; // 记录输入的字符串
 		do {
-			System.out.print(">>");
+			System.out.print("11>>");
 			val = input.nextLine(); // 等待输入值
-			System.out.println("您输入的是：" + val);
+			System.out.println("您输の333入的是：" + val);
 			if ("me".equals(val)) {
 				System.out.println("show me info");
 			} else if ("getChats".equals(val)) {
 				System.out.println("get my all chats list");
 
-			}else if (val.startsWith("addUser")) {
+			} else if (val.startsWith("addUser")) {
 				System.out.println("add User to chat");
-				
-			}  else {
-				System.out.println("未知命令");
+
+			} else {
+				System.out.println("22未知命令");
 			}
 
 		} while (!val.equals("#")); // 如果输入的值不是#就继续输入
