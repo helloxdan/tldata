a5dd3b71dcbbeaa057dac5a1a2b51d00d635d5c3
helloxdan <helloxdan@qq.com>
2018-07-01 01:08:49 +0800

    Signed-off-by: helloxdan <helloxdan@qq.com>

--- src/main/java/com/thinkgem/jeesite/modules/utils/Constants.java
+++ src/main/java/com/thinkgem/jeesite/modules/utils/Constants.java
@@ -17,7 +17,7 @@ public class Constants {
 	public static final String VERIFY_STATUS_NOTPASS = "notpass";// 审核不通过
 	public static final String VERIFY_STATUS_EXPIRED = "expired";// 已过期
 	
-	public static final int PER_SIZE=40;
+	public static final int PER_SIZE=50;//tl 每天最多拉40人，但很多时候给的用户加不上，只能给的值比40大
 
-	public static final double PER_SIZE_DOUBLE=40D;
+	public static final double PER_SIZE_DOUBLE=50D;
 }
