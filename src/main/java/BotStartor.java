71f41a94cef7dd0311777ad4e2c327f3554a31ce
helloxdan <helloxdan@qq.com>
2018-08-03 01:20:35 +0800

    Signed-off-by: helloxdan <helloxdan@qq.com>

--- src/main/java/BotStartor.java
+++ src/main/java/BotStartor.java
@@ -1,3 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -12,15 +18,37 @@ import com.thinkgem.jeesite.modules.tl.service.BotService;
  *
  */
 public class BotStartor {
+	static ApplicationContext ac;
+	static BotService botService;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ApplicationContext ac = new ClassPathXmlApplicationContext(
-				"applicationcontext.xml");
-		BotService botService = (BotService) ac.getBean("botService");
+		// initContext();
+		// 取配置文件信息，得到jobid，拉人总数和采集群信息
+		String cfgFileName = System.getProperty("filename");
+		System.out.println(cfgFileName);
+		File cfgFile=new File(cfgFileName);
+		List<String> lines=new ArrayList<String>();
+		try {
+			lines = FileUtils.readLines(cfgFile);
+			for (String l : lines) {
+				System.out.println(l);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// 卡商编码
+		String cardCode = System.getProperty("cardCode");
+		System.out.println(cardCode);
+	}
 
+	private static void initContext() {
+		ac = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		botService = (BotService) ac.getBean("botService");
 	}
 
 }
